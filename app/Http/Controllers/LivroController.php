d512499c98361d700341f40e578cc84b06d1feb6
Marcos Filipe Ratte Claro <marcosfratte@usp.br>
2020-03-10 12:06:27 -0300

    dia 1 de treinamento

---
+++ app/Http/Controllers/LivroController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LivroController extends Controller
+{
+    public function listar($livro)
+    {
+     return view('livro', compact('livro'));
+    }
+}
